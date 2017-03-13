6a263275eaae1f2327b0dd6347a1e0dbfbbafb69
PennyQ <qianxuran@nao.cas.cn>
2017-03-13 13:23:13 -0400

    add entry point to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 # packages are code source
 
 setup(name='astro_simon',
-      version='0.2dev',
+      version='0.2.dev0',
       description='A simulation monitor for astrophysical N-body simulations',
       url='https://github.com/maxwelltsai/SiMon',
       author='Maxwell Cai, Penny Qian',
@@ -12,4 +12,17 @@ setup(name='astro_simon',
       license='',
       packages=find_packages(),
       zip_safe=False,
-      install_requires=['python-daemon'])
+      install_requires=['python-daemon'],
+      entry_points = {
+      'console_scripts': 
+      ['simon-start = SiMon.simon:daemon',
+      'simon-stop = SiMon.simon:daemon',
+      'simon-interactive = SiMon.simon:interactive'],
+      },
+      # entry_points = """
+#       [console_scripts]
+#       simon-start = SiMon.simon:SiMon.daemon_mode
+#       simon-stop = SiMon.simon:SiMon.daemon_mode
+#       simon-interactive = SiMon.simon:SiMon.interactive_mode
+#       """
+      )
