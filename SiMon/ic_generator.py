dbbf55569e6e68015fbcfe410a440f860522f82a
Maxwell Tsai <maxwellemail@gmail.com>
2017-03-16 13:50:02 +0100

    Refactor IC generator
    Refactor the initial condition generator, rename “pseudo_simulation” to
    “demo_simulation”, amend the restart logic.

---
+++ SiMon/ic_generator.py
@@ -0,0 +1,134 @@
+"""
+Initial condition (IC) generator.
+"""
+import os
+import sys
+try:
+    import configparser as cp  # Python 3 only
+except ImportError:
+    import ConfigParser as cp  # Python 2 only
+
+
+class InitialConditionGenerator(object):
+
+    config_file_template = '''# Per-simulation config file for SiMon
+[Simulation]
+# The name of the simulation code
+Code_name = %s
+
+# The file name of the initial condition input file (for stdin)
+Input_file = %s
+
+# The file name of the simulation log output (for stdout)
+Output_file = %s
+
+# The file name of the simulation error output (for stderr)
+Error_file = %s
+
+# The name of the file used to restart the simulation
+Restart_file = %s
+
+# The timestamp indicating the starting time of the simulation
+Timestamp_started = %s
+
+# The timestamp indicating the last time output files are updated
+Timestamp_last_updated = %s
+
+# The starting time
+T_start = %f
+
+# The termination time (i.e., t_max)
+T_end = %f
+
+# The process ID of the N-body code
+PID = %d
+
+# The priority (i.e. niceness) of the simulation (-20 to 19, lower are higher, same as UNIX systems)
+Niceness = %d
+
+# The shell command to start the simulation
+Start_command: %s
+
+# The shall command to restart the simulation
+Restart_command: %s
+
+# The shall command to stop the simulation
+Stop_command: %s
+
+# The maximum number of times a simulation will be restarted (a simulation is marked as ERROR when exceeding this limit)
+Max_restarts: %d
+    '''
+
+    def __init__(self, simon_dir='.'):
+        self.simon_dir = simon_dir  # the code dir of SiMon
+        self.config_file_global = 'SiMon.conf'  # the global config file name for SiMon
+        self.config_file_per_sim = 'SiMon.conf'  # the per-simulation config file name
+        self.config = None  # the parsed global config instance
+        self.sim_data_dir = None  # simulation data dir
+
+    def parse_config_file(self):
+        conf_fn = os.path.join(self.simon_dir, self.config_file_global)
+        conf = cp.ConfigParser()
+        if os.path.isfile(conf_fn):
+            conf.read(conf_fn)
+            self.config = conf
+            if self.config.has_option('SiMon', 'Root_dir'):
+                self.sim_data_dir = self.config.get('SiMon', 'Root_dir')
+                # check whether the directory exist
+                if not os.path.isdir(self.sim_data_dir):
+                    print('Simulation data directory %s does not exist. Making the dir...' % self.sim_data_dir)
+                    try:
+                        os.mkdir(self.sim_data_dir)
+                        print('Simulation data directory created successfully.')
+                    except IOError as err:
+                        print('Unable to create the simulation data directory: %s' % err)
+                        print('Exiting...')
+                        sys.exit(-1)
+            else:
+                print('Simulation root directory cannot be found in SiMon.conf. Existing...')
+                sys.exit(-1)
+        else:
+            print('Simulation root directory does not exist. Existing...')
+            sys.exit(-1)
+
+    def generate_simulation_ic(self, code_name, t_end, output_dir, start_cmd, input_file=None, output_file=None,
+                               error_file=None, restart_file=None, t_start=0, restart_cmd=None, stop_cmd=None,
+                               niceness=0, max_restarts=5):
+        """
+        Generate the initial condition of a simulation and write it to the given directory.
+        :param code_name: The name of the numerical code, e.g. DemoSimulation
+        :param t_end: The termination time criterion
+        :param output_dir: The directory in which the initial condition files and output files are contained
+        :param start_cmd: The UNIX command to start the simulation
+        :param input_file: The name of the input file (optional)
+        :param output_file: The name of the output file (optional)
+        :param error_file: The name of the error message file (optional)
+        :param restart_file: The name of the restartable checkpoint file (optional)
+        :param t_start: The starting model time (optional, default: 0)
+        :param restart_cmd: The UNIX command to restart the simulation
+        :param stop_cmd: The UNIX command to request the simulation code to stop the simulation (optional)
+        :param niceness: The priority of the simulation, -20 to 19, lower are higher (optional, default: 0)
+        :return: return 0 if succeed, -1 if failed.
+        """
+        if not os.path.isdir(os.path.join(self.sim_data_dir, output_dir)):
+            print('Creating directory: %s' % os.path.join(self.sim_data_dir, output_dir))
+            os.mkdir(os.path.join(self.sim_data_dir, output_dir))
+        conf_file = open(os.path.join(self.sim_data_dir, output_dir, self.config_file_per_sim), 'w')
+        conf_file.write(InitialConditionGenerator.config_file_template % (code_name,
+                                                                          input_file,
+                                                                          output_file,
+                                                                          error_file,
+                                                                          restart_file,
+                                                                          0,  # timestamp started
+                                                                          0,  # timestamp last modified
+                                                                          t_start,
+                                                                          t_end,
+                                                                          0,  # UNIX process ID (PID)
+                                                                          niceness,
+                                                                          start_cmd,
+                                                                          restart_cmd,
+                                                                          stop_cmd,
+                                                                          5))  # max_restarts
+        conf_file.close()
+
+
---
+++ SiMon/module_demo_simulation.py
@@ -0,0 +1,30 @@
+from module_common import SimulationTask
+import re
+import os
+import subprocess
+import time
+import glob
+
+__simulation__ = 'DemoSimulation'
+
+
+class DemoSimulation(SimulationTask):
+
+    def __init__(self, sim_id, name, full_dir, status, mode='daemon', t_min=0, t_max=0, restarts=None):
+        super(DemoSimulation, self).__init__(sim_id, name, full_dir, status, mode, t_min, t_max, restarts)
+
+    def sim_get_model_time(self):
+        super(DemoSimulation, self).sim_get_model_time()
+        orig_dir = os.getcwd()
+        os.chdir(self.full_dir)
+        if self.config.has_option('Simulation', 'Output_file'):
+            output_file = self.config.get('Simulation', 'Output_file')
+            regex = re.compile('\\d+')
+            if os.path.isfile(output_file):
+                last_line = subprocess.check_output(['tail', '-1', output_file])
+                res = regex.findall(last_line)
+                if len(res) > 0:
+                    self.t = float(res[0])
+        os.chdir(orig_dir)
+        return self.t
+
