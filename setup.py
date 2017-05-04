9fdc3379b83a73d5ba23a2cc6a398e47bda48c09
PennyQ <qianxuran@nao.cas.cn>
2017-05-04 10:06:54 -0400

    upload simon to pypi

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 # name is the package name in pip, should be lower case and not conflict with existing packages
 # packages are code source
 
-setup(name='astro_simon',
-      version='0.2.dev0',
+setup(name='astrosimon',
+      version='0.1.0',
       description='A simulation monitor for astrophysical N-body simulations',
       url='https://github.com/maxwelltsai/SiMon',
       author='Maxwell Cai, Penny Qian',
