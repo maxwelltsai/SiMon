d1822a8241abacd7eee43f42b5d3c6f25ea84f72
Maxwell Tsai <maxwellemail@gmail.com>
2017-06-09 15:10:27 +0200

    Various bug fixes
    Fixed the bugs of: 1) ID input crashes the interactive mode because the
    user enter a non-integer value; 2) ID input cannot be reset even if the
    user types N to cancel the input; 3) modules not recognized unless
    placed in the SiMon code dir; 4) SiMon crashed if it cannot find a
    proper module to handle a simulation; 5) package numpy not in the
    dependency list of setup.py; 6) generate_conf() not found.

--- SiMon/utilities.py
+++ SiMon/utilities.py
@@ -1,7 +1,27 @@
 """
-Utility class.
+Implementation of utilities used by SiMon globally.
 """
-import sys
+
+
+config_file_template = """# Global config file for SiMon
+[SiMon]
+
+# The simulation data root directory
+Root_dir: examples/demo_simulations
+
+# The time interval for the SiMon daemon to check all the simulations (in seconds) [Default: 180]
+Daemon_sleep_time: 180
+
+# The number of simulations to be carried out simultaneously [Default: 2]
+Max_concurrent_jobs: 2
+
+# The maximum number of times a simulation will be restarted (a simulation is marked as ERROR when exceeding this limit) [Default: 2]
+Max_restarts: 2
+
+# Log level of the daemon: INFO/WARNING/ERROR/CRITICAL [default: INFO]
+Log_level: INFO
+"""
+
 
 class Utilities(object):
 
@@ -9,13 +29,13 @@ class Utilities(object):
     def progress_bar(val, val_max, val_min=0, prefix='', suffix='', bar_len=30):
         """
         Displays a progress bar in the simulation tree.
-        :param val:
-        :param val_max:
-        :param val_min:
-        :param prefix:
-        :param suffix:
-        :param bar_len:
-        :return:
+        :param val: current value
+        :param val_max: maximum value
+        :param val_min: minimum value
+        :param prefix: marker for the completed part
+        :param suffix: marker for the incomplete part
+        :param bar_len: total length of the progress bar
+        :return: a string representation of the progressbar
         """
         if val_max == 0:
             return ''
@@ -41,31 +61,45 @@ class Utilities(object):
             return '%s%s%s' % (color_codes_bold[colors.index(color)], text, color_codes_bold[colors.index('reset')])
 
     @staticmethod
-    def generate_conf():
-        config_file_template = """
-# Global config file for SiMon
-
-[SiMon]
-
-# The simulation data root directory
-Root_dir: examples/demo_simulations
-
-# The time interval for the SiMon daemon to check all the simulations (in seconds) [Default: 180]
-Daemon_sleep_time: 10
-
-# The number of simulations to be carried out simultaneously [Default: 2]
-Max_concurrent_jobs: 2
-
-# The maximum number of times a simulation will be restarted (a simulation is marked as ERROR when exceeding this limit) [Default: 5]
-Max_restarts: 1
-
-# Log level of the daemon: INFO/WARNING/ERROR/CRITICAL [default: INFO]
-Log_level: INFO
+    def id_input(prompt):
         """
+        Prompt to the user to input the simulation ID (in the interactive mode)
+        """
+        confirmed = False
+        vec_index_selected = []
+        while confirmed is False:
+            response = raw_input(prompt)
+            fragment = response.split(',')
+            for token_i in fragment:
+                if '-' in token_i:  # it is a range
+                    limits = token_i.split('-')
+                    if len(limits) == 2:
+                        try:
+                            if int(limits[0].strip()) < int(limits[1].strip()):
+                                subrange = range(int(limits[0].strip()), int(limits[1].strip())+1)
+                                for j in subrange:
+                                    vec_index_selected.append(j)
+                        except ValueError:
+                            print('Invalid input. Please use only integer numbers.')
+                            continue
+                else:
+                    try:
+                        int(token_i.strip())  # test integer
+                        vec_index_selected.append(token_i.strip())
+                    except ValueError:
+                        print('Invalid input %s. Please use only integer numbers.' % token_i.strip())
+                        continue
+            if raw_input('Your input is \n\t'+str(vec_index_selected)+', confirm? [Y/N] ').lower() == 'y':
+                confirmed = True
+                return map(int, vec_index_selected)
+            else:
+                vec_index_selected = []
+
+    @staticmethod
+    def generate_conf():
         try:
             target = open('SiMon.conf', 'w')
             target.write(config_file_template)
             target.close()
-        except:
+        except IOError:
             print("Unexpected error:", sys.exc_info()[0])
-
